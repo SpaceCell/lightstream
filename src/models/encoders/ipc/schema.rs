385bf269b4ac8d8c04c13b49fcacf651543df9d4
PB <37089506+pbower@users.noreply.github.com>
2025-10-27 06:19:51 +0000

    Minarrow 0.3 compatibility

--- src/models/encoders/ipc/schema.rs
+++ src/models/encoders/ipc/schema.rs
@@ -4,7 +4,7 @@
 //! This includes schema messages, record batches, dictionary batches, and the file footer with block metadata.
 //!
 //! Follows the IPC protocol as outlined
-//! [here](https://arrow.apache.org/docs/format/Columnar.html#serialization-and-interprocess-communication-ipc).
+//! [here](https://arrow.apache.org/docs/format/Columnar.html#serialisation-and-interprocess-communication-ipc).
 //!
 //! Uses the generated FlatBuffers types under `src/arrow/*` (built from `src/flatb/*`).
 
@@ -58,7 +58,6 @@ pub fn build_flatbuf_schema<'a>(
     );
     fbb.finish(msg, None);
     let flatbuffers = fbb.finished_data().to_vec();
-    println!("Schema flatbuffer size: {}", flatbuffers.len());
 
     Ok(flatbuffers)
 }
